3caef178894277ce6919c3f99ff9a8f37d1b03ef
prakharnmehta <prakh.mehta@gmail.com>
2019-10-30 16:11:06 +0530

    Added an html file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
--- products/views.py
+++ products/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
+from .models import CustomUser
+
 def home(request):
-    return render(request,'products/home.html')
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user_check = CustomUser.objects.filter(username=username)
+        print(user_check)
+        if user_check:
+            if user_check[0].password == password:
+                return HttpResponse("Verified Successfully")
+            else:
+                return HttpResponse("Wrong password for the given username")
+        else:
+            return HttpResponse("User doesnt exist!")
+    else:
+        return render(request,'products/LandingPage.html')
